ae6c5b39f296662d11ed6458d41ef36cff907305
unknown <nsathya1990@gmail.com>
2021-02-27 18:04:16 +0530

    integrating font awesome icons

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+
 import classes from './NavigationItems.module.scss';
 
 import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
-  <ul className={classes.NavigationItems}>{/* 
-    <NavigationItem link='/' exact>
-      Home
-    </NavigationItem> */}
+  <ul className={classes.NavigationItems}>
     <NavigationItem link='/products'>Products</NavigationItem>
     <NavigationItem link='/news'>News</NavigationItem>
     <NavigationItem link='/contact'>Contact</NavigationItem>
-    <NavigationItem link='/cart'>Cart</NavigationItem>
+    <NavigationItem link='/cart'>
+      <FontAwesomeIcon icon={faTrashAlt} />
+    </NavigationItem>
   </ul>
 );
 
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,40 +1,68 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faGooglePlusG,
+  faFacebookF,
+  faInstagram,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+
 import Logo from '../Logo/Logo';
 
 import classes from './footer.module.scss';
 
 const Footer = () => (
-    <footer>
-        <div className={classes.FtLogo}>
-            <Logo />
-        </div>
-        <div className={classes.FtMenu}>
-            <ul>
-                <li><Link to="/products">Products</Link></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">Your Cart</a></li>
-            </ul>
-        </div >
-        <div className={classes.FtSocialLegal}>
-            <div className={classes.Social}>
-                <ul>
-                    <li><i className="fab fa-google-plus-g fa-lg"></i></li>
-                    <li><i className="fab fa-facebook-f fa-lg"></i></li>
-                    <li><i className="fab fa-instagram fa-lg"></i></li>
-                    <li><i className="fab fa-twitter fa-lg"></i></li>
-                </ul>
-            </div>
-            <div className={classes.Legal}>
-                <ul>
-                    <li className={classes.PrivacyPolicy}><a href="#">Privacy Policy</a></li>
-                    <li className={classes.Copyrights}>© 2021 Google. All Rights Reserved</li>
-                </ul>
-            </div>
-        </div>
-    </footer>
+  <footer>
+    <div className={classes.FtLogo}>
+      <Logo />
+    </div>
+    <div className={classes.FtMenu}>
+      <ul>
+        <li>
+          <Link to='/products'>Products</Link>
+        </li>
+        <li>
+          <a href='#'>News</a>
+        </li>
+        <li>
+          <a href='#'>Contact</a>
+        </li>
+        <li>
+          <a href='#'>Your Cart</a>
+        </li>
+      </ul>
+    </div>
+    <div className={classes.FtSocialLegal}>
+      <div className={classes.Social}>
+        <ul>
+          <li>
+            <FontAwesomeIcon icon={faGooglePlusG} size='lg' />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faFacebookF} size='lg' />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faInstagram} size='lg' />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faTwitter} size='lg' />
+          </li>
+        </ul>
+      </div>
+      <div className={classes.Legal}>
+        <ul>
+          <li className={classes.PrivacyPolicy}>
+            <a href='#'>Privacy Policy</a>
+          </li>
+          <li className={classes.Copyrights}>
+            © 2021 Google. All Rights Reserved
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
 );
 
 export default Footer;
