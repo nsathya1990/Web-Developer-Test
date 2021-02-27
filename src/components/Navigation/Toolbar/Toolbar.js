1a447af9efbdbff4c668dfd81e2af93a12f89663
unknown <nsathya1990@gmail.com>
2021-02-27 12:48:15 +0530

    adding a sidedrawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,16 +4,18 @@ import classes from './Toolbar.module.scss';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
-    <header className={classes.Toolbar}>
-        <div className={classes.Logo}>
-            <Logo />
-        </div>
-        <nav className={classes.DesktopOnly}>
-            <NavigationItems />
-        </nav>
-    </header>
+  <header className={classes.Toolbar}>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
+  </header>
 );
 
 export default toolbar;
--- src/components/hoc/Layout/Layout.js
+++ src/components/hoc/Layout/Layout.js
@@ -3,14 +3,33 @@ import React, { Component } from 'react';
 import Auxiliary from '../Auxiliary/Auxiliary';
 import Toolbar from '../../Navigation/Toolbar/Toolbar';
 import Footer from '../../Footer/Footer';
+import SideDrawer from '../../Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.module.scss';
 
 class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+
   render() {
     return (
       <Auxiliary>
-        <Toolbar />
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
         <main className={classes.Content}>{this.props.children}</main>
         <Footer />
       </Auxiliary>
